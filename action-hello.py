d8b37c241284600232c7bbabd4363aa7a6a4d073
equisables <equisables@gmail.com>
2019-10-09 10:25:38 +0100

    jai pigé!

--- action-hello.py
+++ action-hello.py
@@ -30,12 +30,16 @@ class Hello(object):
 
         print '[Received] intent: {}'.format(intent_message.intent.intent_name)
 	annee = '{}'.format(intent_message.slots.annee.first().value)
-	if (annee == "1997" or annee == "quand"):
+	if annee == "1997":
 	  say = u"D'après ce que m'ont raconté de vieilles trophistes, 1997 est l'année de naissance du 4L Trophy. J'ai entendu dire qu'elles n'étaient que 3 à concourir cette année là"
+	elif annee == "quand":
+	  say = u"D'après ce que m'ont raconté de vieilles trophistes, 1997 est l'année de naissance du 4L Trophy. J'ai entendu dire qu'elles n'étaient que 3 à concourir cette année la"
 	elif annee == "date de naissance":
 	  say = u"D'après ce que m'ont raconté de vieilles trophistes, 1997 est l'année de naissance du 4L Trophy. J'ai entendu dire qu'elles n'étaient que 3 à concourir cette année la"
-	elif (annee == "2001" or annee == u"commencé"):
+	elif annee == "2001":
 	  say = u"2001, c'est l'année qui marque l'essort du rodetrip. L'école supérieure de commerce de Rennes aide à l'organisation du raid et permet ainsi son développement."
+	elif annee == "commencé":
+	  say = u"2001, c'est l'année qui marque l'essort du rodetrip. L'école supérieure de commecer de Rennes aide à l'organisation du raid et permet ainsi don développement."
 	elif annee == "histoire":
 	  say = u"Le petit prince"
 	elif annee == "2005":
